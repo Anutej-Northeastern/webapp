ee08ea60255d1b83cc24b1af1bf5d9e44a76d25a
Anutej Poddaturi <113458527+apoddaturi@users.noreply.github.com>
2023-02-01 14:08:51 -0500

    Feature/api (#12)
    * checking failed test cases
    
    * changing test
    
    * modifying put method to handle edge case
    
    * testing agent

--- app-service.js
+++ app-service.js
@@ -2,7 +2,7 @@ const client = require('./connection.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const save = async(newUser) => {
+const saveUser = async(newUser) => {
 
     return new Promise((resolve, reject)=>{
         let insertQuery = `insert into users(first_name, last_name, username, password) 
@@ -13,7 +13,7 @@ const save = async(newUser) => {
                 resolve(result.rows[0]);
             }
             else{ 
-                reject('Broke while creating a new user'); 
+                reject('Cannot create a new user'); 
             }
         })
         client.end;
@@ -29,7 +29,6 @@ const hashPassword = (password) => {
             if(!error){
                 bcrypt.hash(password, salt, function(error, hash){
                     if(!error){
-                        console.log(hash);
                         resolve(hash);
                     }else{
                         reject(password);
@@ -50,27 +49,32 @@ const  emailValidation = (input) =>{
     return new Promise((resolve, reject)=>{
         var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
         if(validRegex.test(String(input).toLowerCase())){
-            console.log("valid email");
             resolve(true);
         }else{
-            console.log("invalid email");
             resolve(false);
         }
     })
 }
 
 
-const getUser = async (username) => {
+const fetchUser = async (username, additional) => {
 
     return new Promise((resolve, reject)=>{
         let q = `Select * from users where username = '${username}'`;
         client.query(q, (err, result)=>{
                 if(!err){
-                    console.log(result.rows[0]);
-                    resolve(result.rows[0]);
+                    if(additional === 'check'){
+                        if(result.rows.length>0){
+                           resolve(true)
+                        }else{
+                            resolve(false)
+                        }
+                    }else{
+                        resolve(result.rows[0]);
+                    }    
                 }
                 else{
-                    reject(`something went wrong while trying to query user with username:${username}`);
+                    resolve(false);
                 }
         });
         client.end;
@@ -78,7 +82,23 @@ const getUser = async (username) => {
     
 }
 
-const update = (user, id) => {
+
+
+const checkPasswords = (password, hashedPassword) =>{
+
+    return new Promise((resolve, reject)=>{
+        bcrypt.compare(password, hashedPassword, (error, result)=>{
+            
+            if(result){
+                resolve(true)
+            }else{
+                reject(false)
+            }
+        })
+    })   
+}
+
+const updateUser = (user, id) => {
     var d = new Date();
     d = new Date(d.getTime() - 3000000);
     var date_format_str = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
@@ -88,39 +108,21 @@ const update = (user, id) => {
         let updateQuery = `update users set first_name = '${user.first_name}', last_name = '${user.last_name}', password = '${user.password}', username = '${user.username}', account_updated = timestamp '${date_format_str}' where id = ${id}`;
         client.query(updateQuery, (err, result)=>{
             if(!err){
-                resolve('update successful');
+                resolve(true);
             }
             else{ 
-                reject(`something went wrong while trying to update user details for Id = ${id}`) 
+                reject(false) 
             }
         })
         client.end;
     })   
 }
 
-const comparePasswords = (password, hashedPassword) =>{
-
-    return new Promise((resolve, reject)=>{
-        bcrypt.compare(password, hashedPassword, (error, result)=>{
-            
-            if(result){
-                console.log("true");
-                resolve(true)
-            }else{
-                console.log("false");
-
-                reject(false)
-            }
-        })
-    })   
-}
-
 module.exports = {
-    save,
+    saveUser,
     hashPassword,
     emailValidation,
-    getUser,
-    update,
-    comparePasswords
-    
+    fetchUser,
+    updateUser,
+    checkPasswords
 }
